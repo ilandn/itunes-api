27a0244a78fa535b1993b263245a1aef2bd0d170
ilandn <ilandayans@gmail.com>
2022-09-26 21:28:49 -0500

    bugid: support episodes for lookup CR_by: n/a

--- src/main/java/be/ceau/itunesapi/request/Entity.java
+++ src/main/java/be/ceau/itunesapi/request/Entity.java
@@ -28,36 +28,38 @@ package be.ceau.itunesapi.request;
  */
 public enum Entity {
 
-	ALBUM("album"), 
-	ALL_ARTIST("allArtist"), 
-	ALL_TRACK("allTrack"), 
-	AUDIOBOOK("audiobook"), 
-	AUDIOBOOK_AUTHOR("audiobookAuthor"), 
-	EBOOK("ebook"), 
-	IPAD_SOFTWARE("iPadSoftware"), 
-	MAC_SOFTWARE("macSoftware"),
-	MIX("mix"), 
-	MOVIE("movie"), 
-	MOVIE_ARTIST("movieArtist"), 
-	MUSIC_ARTIST("musicArtist"), 
-	MUSIC_TRACK("musicTrack"), 
-	MUSIC_VIDEO("musicVideo"), 
-	PODCAST("podcast"), 
-	PODCAST_AUTHOR("podcastAuthor"), 
-	SHORT_FILM("shortFilm"), 
-	SHORT_FILM_ARTIST("shortFilmArtist"), 
-	SOFTWARE("software"), 
-	SONG("song"), 
-	TV_EPISODE("tvEpisode"), 
-	TV_SEASON("tvSeason");
+    ALBUM("album"),
+    ALL_ARTIST("allArtist"),
+    ALL_TRACK("allTrack"),
+    AUDIOBOOK("audiobook"),
+    AUDIOBOOK_AUTHOR("audiobookAuthor"),
+    EBOOK("ebook"),
+    IPAD_SOFTWARE("iPadSoftware"),
+    MAC_SOFTWARE("macSoftware"),
+    MIX("mix"),
+    MOVIE("movie"),
+    MOVIE_ARTIST("movieArtist"),
+    MUSIC_ARTIST("musicArtist"),
+    MUSIC_TRACK("musicTrack"),
+    MUSIC_VIDEO("musicVideo"),
+    PODCAST("podcast"),
+    PODCAST_AUTHOR("podcastAuthor"),
+    PODCAST_EPISODE("podcastEpisode"),
+    SHORT_FILM("shortFilm"),
+    SHORT_FILM_ARTIST("shortFilmArtist"),
+    SOFTWARE("software"),
+    SONG("song"),
+    TV_EPISODE("tvEpisode"),
+    TV_SEASON("tvSeason");
 
-	private final String name;
-	private Entity(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
+    private final String name;
+
+    private Entity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
 }
